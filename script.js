9b26f7acd4f857071ce17b88619894d506973815
nico17022001 <nico17022001boolean@gmail.com>
2023-03-29 18:20:28 +0200

    corretto-bonus

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Passaggi 
 1-Creo un collegamento tra la select e js
 2-Creo un ciclo che mi vada a creare 100 box
-3-Creo una funzione dove do i divesi livelli
+3-Creo una funzione che mi cambi i livelli
 */
 
 const button = document.querySelector("button")
@@ -14,17 +14,19 @@ select.addEventListener("change" , function(){
     let contenitoreNumeri = document.createElement("div")
     contenitoreNumeri.classList = "casella hide-number";
     contenitoreNumeri.innerText = i;
+
+
+    contenitoreNumeri.addEventListener('click', function () {
     contenitoreNumeri.classList.add('back-color-box-click');
-    console.log(i)
+      console.log(i)
+    });
 
     if (this.value == 81) {
-
       contenitoreNumeri.classList = "casellaMedia hide-number"
     }
-
     else if(this.value == 49) {
       contenitoreNumeri.classList = "casellaDiff hide-number"
-    } 
+    }
     container.append(contenitoreNumeri)  
   }
 })
