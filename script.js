6b8c561ab18486dec8152fb4025ae8dfa8ba0713
nico17022001 <nico17022001boolean@gmail.com>
2023-03-29 18:09:32 +0200

    fine-bonus

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 /*
 Passaggi 
-1-Creo un collegamento tra il bottone e js
+1-Creo un collegamento tra la select e js
 2-Creo un ciclo che mi vada a creare 100 box
-3-
+3-Creo una funzione dove do i divesi livelli
 */
 
 const button = document.querySelector("button")
 const container = document.querySelector("#gridcontainer")
+const select = document.querySelector("#livelli")
 
-button.addEventListener("click" , generatore )
-
-function generatore(min,max) {
-  for (let i = 1; i <= 100; i++) {
+select.addEventListener("change" , function(){
+  for (let i = 1; i <= this.value; i++) {
     let contenitoreNumeri = document.createElement("div")
     contenitoreNumeri.classList = "casella hide-number";
     contenitoreNumeri.innerText = i;
-    contenitoreNumeri.addEventListener('click', function () {
     contenitoreNumeri.classList.add('back-color-box-click');
     console.log(i)
-    });
-    
+
+    if (this.value == 81) {
+
+      contenitoreNumeri.classList = "casellaMedia hide-number"
+    }
+
+    else if(this.value == 49) {
+      contenitoreNumeri.classList = "casellaDiff hide-number"
+    } 
     container.append(contenitoreNumeri)  
   }
-}
+})
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
